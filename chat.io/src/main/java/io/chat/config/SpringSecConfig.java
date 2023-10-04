ea6e6ef53ebea05737a90e62fce3014194fec181
Farid <faridggl100@gmail.com>
2023-10-04 11:12:31 +0300

    feat: enable OAuth2 resource server

---
+++ chat.io/src/main/java/io/chat/config/SpringSecConfig.java
@@ -0,0 +1,20 @@
+package io.chat.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorize
+                .anyRequest().authenticated()
+                .and().oauth2ResourceServer().jwt();
+
+        return http.build();
+    }
+
+}
